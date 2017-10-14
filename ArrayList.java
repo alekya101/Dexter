3102cbbe7c3a3e59fc304bcc017dfd84ee729f8c
alekya101 <alekyareddy1205@gmail.com>
2017-10-14 16:25:36 -0400

    ArrayList Commit

---
+++ ArrayList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class ArrayListDemo {
+	public static void main(String[] args)
+	{
+	ArrayList l=new ArrayList();
+	l.add("a");
+	l.add(0);
+	l.add("b");
+	System.out.println(l);
+
+}
+}
