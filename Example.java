161212ea6db89c67c0992d1a5e47c3ddab5a0599
alekya101 <alekyareddy1205@gmail.com>
2017-10-14 15:58:27 -0400

    Example1 is modified

---
+++ Example.java
@@ -0,0 +1,42 @@
+package com.abc;
+
+import java.util.Properties;    
+import javax.mail.*;    
+import javax.mail.internet.*;    
+class Mailer{  
+    public static void send(String from,String password,String to,String sub,String msg){  
+          //Get properties object    
+          Properties props = new Properties();    
+          props.put("mail.smtp.host", "smtp.gmail.com");    
+          props.put("mail.smtp.socketFactory.port", "465");    
+          props.put("mail.smtp.socketFactory.class",    
+                    "javax.net.ssl.SSLSocketFactory");    
+          props.put("mail.smtp.auth", "true");    
+          props.put("mail.smtp.port", "465");    
+          //get Session   
+          Session session = Session.getDefaultInstance(props,    
+           new javax.mail.Authenticator() {    
+           protected PasswordAuthentication getPasswordAuthentication() {    
+           return new PasswordAuthentication(from,password);  
+           }    
+          });    
+          //compose message    
+          try {    
+           MimeMessage message = new MimeMessage(session);    
+           message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));    
+           message.setSubject(sub);    
+           message.setText(msg);    
+           //send message  
+           Transport.send(message);    
+           System.out.println("message sent successfully");    
+          } catch (MessagingException e) {throw new RuntimeException(e);}    
+             
+    }  
+}  
+public class Example{    
+ public static void main(String[] args) {    
+     //from,password,to,subject,message  
+     Mailer.send("alekyareddy1205@gmail.com","akila169","alekya101@gmail.com","details","alekya,6602382926,alekya101@gmail.com");  
+     //change from, password and to  
+ }    
+}    
